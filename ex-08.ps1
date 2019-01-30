3cc704522c704c2f1af499ca9375b28f4041eaff
movinalot <john@movinalot.com>
2019-01-30 15:44:09 +0100

    remov emdash

--- ex-08.ps1
+++ ex-08.ps1
@@ -6,11 +6,11 @@ Import-Module Cisco.UCSManager
 Connect-Ucs -Name 198.18.133.91
 
 $ucsCred = Get-Credential
-$ucsSource = Connect-Ucs -Name 198.18.133.91 -Credential $ucsCred –NotDefault
-$ucsTarget = Connect-Ucs -Name 198.18.134.249 -Credential $ucsCred –NotDefault
+$ucsSource = Connect-Ucs -Name 198.18.133.91 -Credential $ucsCred -NotDefault
+$ucsTarget = Connect-Ucs -Name 198.18.134.249 -Credential $ucsCred -NotDefault
 
 Compare-UcsMo $(Get-UcsVlan -Ucs $ucsTarget) $(Get-UcsVlan -Ucs $ucsSource)
 
-Sync-UcsMo -Ucs $ucsTarget $(Compare-UcsMo $(Get-UcsVlan -Ucs $ucsTarget) $(Get-UcsVlan -Ucs $ucsSource)) -WhatIf –DeleteNotPresent
+Sync-UcsMo -Ucs $ucsTarget $(Compare-UcsMo $(Get-UcsVlan -Ucs $ucsTarget) $(Get-UcsVlan -Ucs $ucsSource)) -WhatIf -DeleteNotPresent
 
 Disconnect-Ucs
