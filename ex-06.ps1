a269900d4bcdf9cd5b96768209b0168145d72a0d
movinalot <john@movinalot.com>
2019-01-30 15:42:39 +0100

    Remove emdash

--- ex-06.ps1
+++ ex-06.ps1
@@ -10,11 +10,11 @@ Get-UcsLanCloud | Add-UcsVlan -Name CiscoLive -Id 500
 
 # Add an Organization
 $ucsOrg = Add-UcsOrg -Name CiscoLiveOrg
-Add-UcsServiceProfile –Org $ucsOrg -Name CiscoLiveSP
-Add-UcsBootPolicy –Org $ucsOrg -Name CL_BP -RebootOnUpdate yes -BootMode legacy | Add-UcsLsbootVirtualMedia -ModifyPresent -Access "read-only-local" -LunId "0" -Order 1
+Add-UcsServiceProfile -Org $ucsOrg -Name CiscoLiveSP
+Add-UcsBootPolicy -Org $ucsOrg -Name CL_BP -RebootOnUpdate yes -BootMode legacy | Add-UcsLsbootVirtualMedia -ModifyPresent -Access "read-only-local" -LunId "0" -Order 1
 
 Get-UcsServiceProfile -Name CiscoLiveSP | Set-UcsServiceProfile -BootPolicyName CL_BP -Force
-Add-UcsServiceProfile –Org $ucsOrg -Name CiscoLiveSP -BootPolicyName CL_BP -ModifyPresent
+Add-UcsServiceProfile -Org $ucsOrg -Name CiscoLiveSP -BootPolicyName CL_BP -ModifyPresent
 Get-UcsServiceProfile -Name CiscoLiveSP
 
 Get-UcsOrg -Name CiscoLiveOrg | Get-UcsBootPolicy -Name CL_BP | Remove-UcsBootPolicy -Force
